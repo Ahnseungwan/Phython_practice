0dd49f578a491e5d6e478e53beba72f5b3a2ec2d
Ahnseungwan <fish14644@naver.com>
2021-01-05 21:42:07 +0900

    Practice

---
+++ 1.5/1.5 review.py
@@ -0,0 +1,25 @@
+# string = input("인사말을 입력하세요 ")
+# print(string)
+
+# number = input("숫자를 입력하세요")
+# print(number)
+# output = "{:5d}".format(52)
+# outputa = "{:05d}".format(52)
+# print(output)
+# print(outputa)
+# name = "ahnseungwan"
+# name_u = name.upper()
+# print(name_u)
+# number = input("정수입력 ")
+# number = int(number)
+# if number > 0:
+#     print("양수입니다.")
+# import datetime
+# now = datetime.datetime.now()
+
+number = input("숫자를 입력하시오. ")
+number = int(number)
+if number % 2 == 0:
+    print("짝수 입니다.")
+else :
+    print("홀수 입니다.")
