af99e9f3f22f05c041a07c3adfa56fe30462b7f0
Ahnseungwan <fish14644@naver.com>
2021-02-04 22:44:37 +0900

    test

---
+++ 2021.01/1.15/dd.py
@@ -0,0 +1,4 @@
+# A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(A[-6:])
+List = [5 if i > 4 else i for i in range(10)]
+print(List)
---
+++ school/2.1.py
@@ -0,0 +1,6 @@
+# print("안녕하세요")
+# print("intput : ")
+int(a = input("입력하세요 :"))
+int(b = input("입력하세요 : "))
+
+print(a+b)
